6aa6f7401a9561b70cc87517a6a83d6dd22d6615
Nourhan Elhakim <nourhanmagdy993@gmail.com>
2020-06-08 19:54:04 +0100

    Drop removed booking_controller require in app.rb

--- app.rb
+++ app.rb
@@ -1,6 +1,5 @@
 require( 'sinatra' )
 require( 'sinatra/contrib/all' )
-require_relative('controllers/booking_controller')
 require_relative('controllers/member_controller')
 require_relative('controllers/sparta_class_controller')
 
